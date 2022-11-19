967c41a3fec2d4ee8cddee6101bceb9889e321e2
Rama <Rama12335@example.com>
2022-11-19 17:23:08 +0700

    first commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,39 +25,6 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="username" class="col-md-4 col-form-label text-md-end">{{_('username')}}</label>
-
-                            <div class="col-md-6">
-                                <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
-
-                                @error('username')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="lavel" class="col-md-4 col-form-label text-md-end">{{ __('lavel') }}</label>
-
-                            <div class="col-md-6">
-                                  <!--<input id="lavel" type="textl" class="form-control @error('lavel') is-invalid @enderror" name="lavel" value="{{ old('lavel') }}" required autocomplete="lavel"> -->
-                                  <select class="form-select @error('lavel') is-invalid @enderror" aria-label="pilih lavel" name="lavel">
-                                    <option selected>pilih lavel</option>
-                                    <option value="admin">admin</option>
-                                    <option value="editor">editor</option>
-                                  </select>
-                                  @error('lavel')
-                                  <span class="invalid-feedback" role="alert">
-                                      <strong>{{ $message }}</strong>
-                                  </span>
-                                  @enderror
-                            </div>
-                        </div>
-
-        
-                        <div class="row mb-3">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
